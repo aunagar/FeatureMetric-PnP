250950cba68629d0028e0034aa6493e1060f2b0d
Ajay Unagar <ajayunagar@gmail.com>
2020-04-05 18:21:45 +0200

    minor error

--- S2DHM/s2dhm/pose_prediction/sparse_to_dense_predictor.py
+++ S2DHM/s2dhm/pose_prediction/sparse_to_dense_predictor.py
@@ -11,7 +11,7 @@ from pose_prediction import keypoint_association
 from pose_prediction import exhaustive_search
 from visualization import plot_correspondences
 
-from optimizer_PnP import optimize
+from pose_prediction import optimizer_PnP
 
 
 @gin.configurable
@@ -117,7 +117,7 @@ class SparseToDensePredictor(predictor.PosePredictor):
             if len(predictions):
                 export, best_prediction = self._choose_best_prediction(
                     predictions, query_image)
-                matrix, quaterion = optimize(query_hypercolumns.view(channels, width, height)[None, ...],
+                matrix, quaterion = optimizer_PnP.optimize(query_hypercolumns.view(channels, width, height)[None, ...],
                                     self._network, best_prediction, intrinsics)
                 export[1], export[2] = quaterion, matrix
 
