06dc21a9e72a18bf56c6db6ac33a436afde3febd
ntselepidis <ntselepidis@student.ethz.ch>
2020-04-05 22:24:31 +0200

    Fixes.

--- S2DHM/s2dhm/pose_prediction/optimizer_PnP.py
+++ S2DHM/s2dhm/pose_prediction/optimizer_PnP.py
@@ -7,22 +7,21 @@ from pose_prediction import matrix_utils
 # this is a hack
 sys.path.insert(0, os.path.abspath('../../'))
 
-
 from BA.featureBA.src.model import sparse3DBA
 from BA.featureBA.src.utils import sobel_filter
 
 @gin.configurable
 def optimize(query_hypercolumns, net, prediction, K, image_shape):
-
-    reference_hypercolumns, image_shape = net.compute_hypercolumn( [prediction.reference_filename], to_cpu=False, resize=True )
+    
+    reference_hypercolumns, _ = net.compute_hypercolumn( [prediction.reference_filename], to_cpu=False, resize=True )
     relative_shape = np.array([reference_hypercolumns.shape[2] / image_shape[0], reference_hypercolumns.shape[3] / image_shape[1]])
 
+    K = torch.from_numpy( K )
     pts3D = torch.from_numpy( prediction.points_3d.reshape(-1,3) ) 
     ref2d = torch.from_numpy(relative_shape).view(1,2) * torch.from_numpy(prediction.reference_inliers)
-    print( ref2d.size() )
     ref2d = torch.flip( ref2d.type(torch.IntTensor), (1,) )
 
-    feature_ref = torch.cat([reference_hypercolumn.squeeze(0)[:, i, j].unsqueeze(0) for i, j in zip(ref2d[:,0], ref2d[:,1])]).type(torch.DoubleTensor)
+    feature_ref = torch.cat([reference_hypercolumns.squeeze(0)[:, i, j].unsqueeze(0) for i, j in zip(ref2d[:,0], ref2d[:,1])]).type(torch.DoubleTensor)
     feature_map_query = query_hypercolumns.squeeze(0).type(torch.DoubleTensor)
     # T_init = filename_to_pose['/'.join(ref_images[0].split('/')[-3:])][1]
     T_init = prediction.matrix
