41335d9a66b4e5dd1707edc734930535d88f3107
Ajay Unagar <ajayunagar@gmail.com>
2020-04-05 13:51:17 +0200

    full file

--- S2DHM/s2dhm/run_featurePnP.py
+++ S2DHM/s2dhm/run_featurePnP.py
@@ -72,20 +72,29 @@ if __name__ == '__main__':
     image_root = DATA_PATH + 'images/'
 
     # if no file is provided in args, we will use our own!
-    ref_image = 'overcast-reference/rear/1417176495903575.jpg'
-    query_image = 'overcast-reference/rear/1417176509083576.jpg'
+    ref_images = ['overcast-reference/rear/1417176495903575.jpg',
+                'overcast-reference/rear/1417176586163507.jpg',
+                'overcast-reference/rear/1417176991015329.jpg',
+                'overcast-reference/rear/1417177488763211.jpg',
+                'overcast-reference/rear/1417177952697851.jpg']
+    query_images = ['overcast-reference/rear/1417176509083576.jpg',
+                'overcast-reference/rear/1417176586345356.jpg',
+                'overcast-reference/rear/1417176991197053.jpg',
+                'overcast-reference/rear/1417177489035919.jpg',
+                'overcast-reference/rear/1417177952879702.jpg']
+
 
     if args.ref_image:
         ref_images = [image_root + args.ref_image]
     else:
-        print("using default image : {}".format(ref_image))
-        ref_images = [image_root + ref_image]
+        print("using default images")
+        ref_images = [image_root + ref_image for ref_image in ref_images]
 
     if args.query_image:
         query_images = [image_root + args.query_image]
     else:
-        print("using default image : {}".format(query_image))
-        query_images = [image_root + query_image]
+        print("using default images")
+        query_images = [image_root + query_image for query_image in query_images]
 
     filenames = ref_images + query_images
 
@@ -99,91 +108,94 @@ if __name__ == '__main__':
     net = network.ImageRetrievalModel(device = "cpu")
     s2dPnP = SparseToDenseFeatureMetricPnP(filename_to_pose, filename_to_intrinsics,
             filename_to_local_reconstruction, net)
-    prediction, query_hypercolumn, reference_hypercolumn = s2dPnP.run(query_images[0], ref_images[0])
-    
-    del filename_to_local_reconstruction
-    # query_hypercolumn = interpolate(query_hypercolumn, size=(1024,1024),
-    #                                 mode = 'bilinear', align_corners=True)
-    # reference_hypercolumn = interpolate(reference_hypercolumn, size = (1024,1024),
-    #                                     mode='bilinear', align_corners = True)
-    
-    print("iterpolated Hypercolumn size is {}".format(query_hypercolumn.shape))
 
-    result = dict()
-    result['prediction'] = prediction
-    # result['query_hypercolumn'] = query_hypercolumn.numpy()
-    # result['reference_hypercolumn'] = reference_hypercolumn.numpy()
+    for k in range(len(query_images)):
+        prediction, query_hypercolumn, reference_hypercolumn = s2dPnP.run(query_images[k], ref_images[k])
+        
+        # del filename_to_local_reconstruction
+        # query_hypercolumn = interpolate(query_hypercolumn, size=(1024,1024),
+        #                                 mode = 'bilinear', align_corners=True)
+        # reference_hypercolumn = interpolate(reference_hypercolumn, size = (1024,1024),
+        #                                     mode='bilinear', align_corners = True)
+        
+        print("Hypercolumn size is {}".format(query_hypercolumn.shape))
 
-    # np.save(args.result + "query_hypercolumn", query_hypercolumn.numpy())
-    # np.save(args.result + "reference_hypercolumn", reference_hypercolumn.numpy())
-    
+        result = dict()
+        result['prediction'] = prediction
+        # result['query_hypercolumn'] = query_hypercolumn.numpy()
+        # result['reference_hypercolumn'] = reference_hypercolumn.numpy()
 
-
-    ### For plotting
-    q_img = cv2.imread(query_images[0])
-    r_img = cv2.imread(ref_images[0])
-
-    for i, p in enumerate(prediction.reference_inliers.astype(int)):
-        # print(i, p)
-        cv2.circle(r_img, tuple(p), 1, (128, 128, 0), -1)
-
-    cv2.imwrite(args.result + 'reference_detection.png', r_img)
-
-    # for i, p in enumerate(prediction.query_inliers.astype(int)):
-    #     print(i, p)
-    #     cv2.circle(q_img, tuple(p), 1, (128, 128, 0), -1)
-
-    # cv2.imwrite(args.result + 'query_detection.png', q_img)
-
-    pts3D = torch.from_numpy(prediction.points_3d.reshape(-1,3))
-    ref2d = torch.flip(torch.from_numpy((1/8*prediction.reference_inliers).astype(int)),(1,))
-    feature_ref = torch.cat([reference_hypercolumn.squeeze(0)[:, i, j].unsqueeze(0) for i, j in zip(ref2d[:,0],
-                            ref2d[:,1])]).type(torch.DoubleTensor)
-    feature_map_query = query_hypercolumn.squeeze(0).type(torch.DoubleTensor)
-    # T_init = filename_to_pose['/'.join(ref_images[0].split('/')[-3:])][1]
-    T_init = prediction.matrix
-    R_init, t_init = torch.from_numpy(T_init[:3, :3]), torch.from_numpy(T_init[:3,3])
-    feature_grad_x, feature_grad_y = sobel_filter(feature_map_query)
-    K = torch.from_numpy(filename_to_intrinsics[ref_images[0]][0]).type(torch.DoubleTensor)
-
-    print("3D points shape is {}".format(pts3D.size()))
-    print("reference features shape is {}".format(feature_ref.size()))
-    print("query feature map is of size {}".format(feature_map_query.size()))
-    print("Rotation matrix is of shape {} and translation {}".format(R_init.size(), t_init.size()))
-    print("feature gradient is of shape {}".format(feature_grad_x.size()))
-    # inital projection and plotting
-    proj2d = torch.mm(R_init, pts3D.T).T + t_init
-    proj2d = torch.mm(K, proj2d.T).T
-    proj2d = proj2d/proj2d[:,-1,None]
-    proj2d = torch.round(proj2d[:,:2]).type(torch.IntTensor)-1
-
-    for i, p in enumerate(proj2d):
-        # print(i, p)
-        cv2.circle(q_img, tuple(p), 1, (128, 128, 0), -1)
-
-    cv2.imwrite(args.result + 'query_initialization.png', q_img)
+        # np.save(args.result + "query_hypercolumn", query_hypercolumn.numpy())
+        # np.save(args.result + "reference_hypercolumn", reference_hypercolumn.numpy())
+        
 
 
+        ### For plotting
+        q_img = cv2.imread(query_images[k])
+        r_img = cv2.imread(ref_images[k])
 
-    model = sparse3DBA(n_iters = 5000, lambda_ = 0.1, verbose=True)
-    R, t = model(pts3D, feature_ref, feature_map_query, feature_grad_x, feature_grad_y, K, 1024, 1024,R_init, t_init)
+        for i, p in enumerate(prediction.reference_inliers.astype(int)):
+            # print(i, p)
+            cv2.circle(r_img, tuple(p), 1, (128, 128, 0), 5)
 
-    result['R'] = R.numpy()
-    result['t'] = t.numpy()
-    pickle.dump(result, open(args.result + "prediction.p", 'wb'))
+        cv2.imwrite(args.result + 'ref_'+ str(k) + '_detection.png', r_img)
 
-    # final projection and plotting
-    q_img = cv2.imread(query_images[0])
-    proj2d = torch.mm(R, pts3D.T).T + t
-    proj2d = torch.mm(K, proj2d.T).T
-    proj2d = proj2d/proj2d[:,-1,None]
-    proj2d = torch.round(proj2d[:,:2]).type(torch.IntTensor)-1
+        for i, p in enumerate(prediction.query_inliers.astype(int)):
+            print(i, p)
+            cv2.circle(q_img, tuple(p), 1, (128, 128, 0), 5)
 
-    for i, p in enumerate(proj2d):
-        # print(i, p)
-        cv2.circle(q_img, tuple(p), 1, (128, 128, 0), -1)
+        cv2.imwrite(args.result + 'query_'+ str(k) + '_detection.png', q_img)
 
-    cv2.imwrite(args.result + 'query_final.png', q_img)
+        pts3D = torch.from_numpy(prediction.points_3d.reshape(-1,3))
+        ref2d = torch.flip(torch.from_numpy((1/8*prediction.reference_inliers).astype(int)),(1,))
+        feature_ref = torch.cat([reference_hypercolumn.squeeze(0)[:, i, j].unsqueeze(0) for i, j in zip(ref2d[:,0],
+                                ref2d[:,1])]).type(torch.DoubleTensor)
+        feature_map_query = query_hypercolumn.squeeze(0).type(torch.DoubleTensor)
+        # T_init = filename_to_pose['/'.join(ref_images[0].split('/')[-3:])][1]
+        T_init = prediction.matrix
+        R_init, t_init = torch.from_numpy(T_init[:3, :3]), torch.from_numpy(T_init[:3,3])
+        feature_grad_x, feature_grad_y = sobel_filter(feature_map_query)
+        K = torch.from_numpy(filename_to_intrinsics[ref_images[k]][0]).type(torch.DoubleTensor)
+
+        print("3D points shape is {}".format(pts3D.size()))
+        print("reference features shape is {}".format(feature_ref.size()))
+        print("query feature map is of size {}".format(feature_map_query.size()))
+        print("Rotation matrix is of shape {} and translation {}".format(R_init.size(), t_init.size()))
+        print("feature gradient is of shape {}".format(feature_grad_x.size()))
+        # inital projection and plotting
+        proj2d = torch.mm(R_init, pts3D.T).T + t_init
+        proj2d = torch.mm(K, proj2d.T).T
+        proj2d = proj2d/proj2d[:,-1,None]
+        proj2d = torch.round(proj2d[:,:2]).type(torch.IntTensor)-1
+
+        # inital points
+        q_img = cv2.imread(query_images[k])
+
+        for i, p in enumerate(proj2d):
+            # print(i, p)
+            cv2.circle(q_img, tuple(p), 1, (128, 128, 0), 3)
+
+        cv2.imwrite(args.result + 'query_' + str(k) + '_initialization.png', q_img)
+
+        model = sparse3DBA(n_iters = 500, lambda_ = 0.1, verbose=True)
+        R, t = model(pts3D, feature_ref, feature_map_query, feature_grad_x, feature_grad_y, K, 1024, 1024,R_init, t_init)
+
+        result['R'] = R.numpy()
+        result['t'] = t.numpy()
+        pickle.dump(result, open(args.result +str(k) + "_prediction.p", 'wb'))
+
+        # final projection and plotting
+        q_img = cv2.imread(query_images[k])
+        proj2d = torch.mm(R, pts3D.T).T + t
+        proj2d = torch.mm(K, proj2d.T).T
+        proj2d = proj2d/proj2d[:,-1,None]
+        proj2d = torch.round(proj2d[:,:2]).type(torch.IntTensor)-1
+
+        for i, p in enumerate(proj2d):
+            # print(i, p)
+            cv2.circle(q_img, tuple(p), 1, (128, 128, 0), -1)
+
+        cv2.imwrite(args.result + 'query_' + str(k) + '_final.png', q_img)
 
     del filename_to_pose, filename_to_intrinsics, query_hypercolumn, reference_hypercolumn
 
