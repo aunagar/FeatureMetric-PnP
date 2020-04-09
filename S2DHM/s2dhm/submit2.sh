8db40fcd550fc04220b04a0c925a5e6f38ca0dd5
Ajaykumar Unagar <aunagar@lo-login-02.leonhard.ethz.ch>
2020-04-09 09:21:20 +0200

    minor changes

--- S2DHM/s2dhm/submit2.sh
+++ S2DHM/s2dhm/submit2.sh
@@ -1,1 +1,1 @@
-bsub -W 48:00 -n 1 -R "rusage[mem=32768,ngpus_excl_p=1]" -oo output.txt python run.py --dataset robotcar --mode sparse_to_dense
+bsub -W 12:00 -n 1 -R "rusage[mem=12768,ngpus_excl_p=1]" -oo output_night_rain.txt python run.py --dataset robotcar --mode sparse_to_dense
