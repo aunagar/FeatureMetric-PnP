4fb6fff88ac60e32069fecd7da5ef72ba023845a
Ajaykumar Unagar <aunagar@lo-login-02.leonhard.ethz.ch>
2020-04-12 22:01:24 +0200

    different testing

--- S2DHM/s2dhm/submit2.sh
+++ S2DHM/s2dhm/submit2.sh
@@ -1,1 +1,1 @@
-bsub -W 12:00 -n 1 -R "rusage[mem=12768,ngpus_excl_p=1]" -oo output_night_rain.txt python run.py --dataset robotcar --mode sparse_to_dense
+bsub -W 12:00 -n 1 -R "rusage[mem=32768,ngpus_excl_p=1]" -oo output_night_rain_9_17_21_24_28.txt python run.py --dataset robotcar --mode sparse_to_dense
