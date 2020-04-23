4c0e73be6c94896e31da205640527833c1533c29
Ajay Unagar <ajayunagar@gmail.com>
2020-04-23 23:22:08 +0200

    scale should not be fixed in the code

--- S2DHM/s2dhm/run_featurePnP.py
+++ S2DHM/s2dhm/run_featurePnP.py
@@ -198,7 +198,9 @@ if __name__ == '__main__':
         cv2.imwrite(args.result + 'query_'+ str(k) + '_detection.png', q_img)
 
         pts3D = torch.from_numpy(prediction.points_3d.reshape(-1,3))
-        ref2d = torch.flip(torch.from_numpy((1/8*prediction.reference_inliers).astype(int)),(1,))
+        scale = reference_hypercolumn.shape[-1]/r_img.shape[1]
+        print(scale)
+        ref2d = torch.flip(torch.from_numpy((scale*prediction.reference_inliers).astype(int)),(1,))
         feature_ref = torch.cat([reference_hypercolumn.squeeze(0)[:, i, j].unsqueeze(0) for i, j in zip(ref2d[:,0],
                                 ref2d[:,1])]).type(torch.DoubleTensor)
         feature_map_query = query_hypercolumn.squeeze(0).type(torch.DoubleTensor)
