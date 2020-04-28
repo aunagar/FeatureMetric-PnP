caad2118cd798dcd2c87b944809f6ca065f741c0
ntselepidis <ntselepidis@student.ethz.ch>
2020-04-28 10:36:30 +0200

    CMU scripts setup.

--- S2DHM/s2dhm/submit_cmu.sh
+++ S2DHM/s2dhm/submit_cmu.sh
@@ -1,1 +1,2 @@
-bsub -W 60:00 -n 1 -R "rusage[mem=32768,ngpus_excl_p=1]" -oo output_cmu_slice2.txt python run.py --dataset cmu --mode sparse_to_dense
+CMU_SLICE=$1
+bsub -W 60:00 -n 1 -R "rusage[mem=32768,ngpus_excl_p=1]" -oo output_cmu_slice_$CMU_SLICE.txt python run.py --dataset cmu --mode sparse_to_dense --cmu_slice $CMU_SLICE
