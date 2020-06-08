902d5ed1d83aac43420c113c54b753f84de9625c
ntselepidis <ntselepidis@student.ethz.ch>
2020-06-08 02:54:51 +0200

    Added bash script for automatic job submission of cmu subslices.

---
+++ submit_cmu_auto.sh
@@ -0,0 +1,37 @@
+#/bin/bash
+CORES=2
+CMU_SLICE=13
+NumImages=2
+SubSliceSize=2
+DivSubSlices=$(( $NumImages / $SubSliceSize))
+ModSubSlices=$(( $NumImages % $SubSliceSize))
+
+# First batch
+if [ $ModSubSlices -gt 0 ]
+then
+    start=0
+    end=$(($ModSubSlices-1))
+    echo $start $end
+    bsub -n $CORES -W 00:05 -R 'rusage[mem=32768]' -J "cmu_"$CMU_SLICE"_"$start"_"$end -oo "output_cmu_"$CMU_SLICE"_"$start"_"$end".txt" \
+        python run.py \
+        --dataset=cmu \
+        --input_config=input_configs/default_cmu_subset_$CMU_SLICE.gin \
+        --cmu_slice=$CMU_SLICE \
+        --start=$start \
+        --end=$end
+fi
+
+# Next batches
+for ((i=$ModSubSlices; i<$NumImages; i=$(($i+$SubSliceSize))))
+do
+    start=$i
+    end=$(($i+$SubSliceSize-1))
+    echo $start $end
+    bsub -n $CORES -W 00:05 -R 'rusage[mem=32768]' -J "cmu_"$CMU_SLICE"_"$start"_"$end -oo "output_cmu_"$CMU_SLICE"_"$start"_"$end".txt" \
+        python run.py \
+        --dataset=cmu \
+        --input_config=input_configs/default_cmu_subset_$CMU_SLICE.gin \
+        --cmu_slice=$CMU_SLICE \
+        --start=$start \
+        --end=$end
+done
